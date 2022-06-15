6c4daaa0ea5dcb1bdbe7b680cc3ccf0a21db7291
Kurt <kurt@ilinke.com>
2022-06-15 23:58:18 +0800

    add prettier, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier'
+  ],
+  plugins: ['vue', 'prettier', '@typescript-eslint'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prefer-const': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ],
+    'prettier/prettier': 'error'
+  },
+  ignorePatterns: ['dist']
+}
