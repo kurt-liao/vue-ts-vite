ca92367883acb17132a3c7b7ad63013ae1b31d87
kurt <vermouth0226@gmail.com>
2022-06-18 22:03:03 +0800

    add element plus auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Components from "unplugin-vue-components/vite";
+import Components from 'unplugin-vue-components/vite'
 import * as path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,8 +12,22 @@ export default defineConfig({
       '@/': `${path.resolve(__dirname, 'src')}/`
     }
   },
-  plugins: [vue(), Components({
-    dirs: ["src/components"],
-    dts: "src/components.d.ts"
-  })]
+  plugins: [
+    vue(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      dts: true,
+      imports: ['vue', 'vue-router']
+    }),
+    Components({
+      dirs: ['src/components'],
+      dts: true,
+      resolvers: [ElementPlusResolver()]
+    })
+  ]
 })
