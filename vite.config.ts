81801d235474f1d2febf09e57509d283949cccc2
kurt <vermouth0226@gmail.com>
2022-06-18 23:10:29 +0800

    add icons

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import Components from 'unplugin-vue-components/vite'
 import * as path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,12 +24,24 @@ export default defineConfig({
         /\.md$/ // .md
       ],
       dts: true,
-      imports: ['vue', 'vue-router']
+      imports: [
+        'vue',
+        'vue-router',
+        {
+          '@vueuse/core': [],
+          axios: [
+            // default imports
+            ['default', 'axios'] // import { default as axios } from 'axios',
+          ]
+        }
+      ],
+      resolvers: [ElementPlusResolver()]
     }),
     Components({
       dirs: ['src/components'],
       dts: true,
-      resolvers: [ElementPlusResolver()]
-    })
+      resolvers: [ElementPlusResolver(), IconsResolver()]
+    }),
+    Icons({ compiler: 'vue3', autoInstall: true })
   ]
 })
