59ab18b5ae356c7e3235f6caffc276d7281834e4
kurt <vermouth0226@gmail.com>
2022-06-18 22:20:58 +0800

    test auto import & element plus

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-console.log('123')
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+
+  <!-- use element plus components without import manually-->
+  <!-- component you have used will define in 'component.d.ts' file -->
+  <el-main>
+    <el-button type="primary">Click</el-button>
+  </el-main>
 </template>
 
 <style>
