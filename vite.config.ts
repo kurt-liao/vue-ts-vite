55f2c15f4c5b162d6a89e10e798724c2d6505d84
kurt <vermouth0226@gmail.com>
2022-06-18 21:52:48 +0800

    add path resolver, component resolver

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from "unplugin-vue-components/vite";
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`
+    }
+  },
+  plugins: [vue(), Components({
+    dirs: ["src/components"],
+    dts: "src/components.d.ts"
+  })]
 })
